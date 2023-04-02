4102f939cb5d642c588fd5f4a8cad2be49cafd79
lbarjak <barjak.laszlo@gmail.com>
2023-04-02 14:11:34 +0200

    init

---
+++ Main.js
@@ -0,0 +1,60 @@
+import { stations } from './stations.js'
+
+export default class Main {
+
+    constructor() {
+        window.onresize = this.reload
+        this.width = window.innerWidth
+        this.height = window.innerHeight
+        this.drawing = SVG()
+            .addTo('#svgs')
+            .size(this.width, this.height)
+        this.rect = this.drawing
+            .rect(this.width, this.height)
+            .attr({ fill: 'lightgray' })
+        //.stroke({ color: '#f06', opacity: 0.6, width: 5 })
+        console.log(this.rect.height())
+
+        //this.main()
+        this.map()
+    }
+
+    reload = () => {
+        fetch('', {
+            'Cache-Control': 'no-cache'
+        })
+            .then(() => location.reload())
+            .catch((error) => console.warn(error))
+    }
+
+    main() {
+        let Lan0 = 45.8
+        let Lon0 = 16.083333
+        let keys = Object.keys(stations)
+        let value
+        for (let key of keys) {
+            value = stations[key]
+            console.log(key + " " + value[0] + " " + (value[1] - Lan0) + " " + (value[2] - Lon0))
+        }
+    }
+
+    map() {
+        let mapHeight
+        let mapWidth
+        let ratioSvg = this.width / this.height
+        const ratioHu = 1.625
+        let x = 0
+        let y = 0
+        if (ratioSvg >= ratioHu) {
+            mapHeight = this.height
+            mapWidth = mapHeight * ratioHu
+            x = (this.width - mapWidth) / 2
+        } else {
+            mapWidth = this.width
+            mapHeight = mapWidth / ratioHu
+            y = (this.height - mapHeight) / 2
+        }
+
+        let map = this.drawing.image('./HU_counties_blank-teszt.svg').size(mapWidth, mapHeight).x(x).y(y)
+    }
+}
