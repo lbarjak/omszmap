4dff9b22f2c41d121532611317bd8b2337cdd84a
lbarjak <barjak.laszlo@gmail.com>
2023-04-02 20:29:25 +0200

    skeleton of Station

--- Station.js
+++ Station.js
@@ -1,3 +1,30 @@
 export default class Station {
 
+    constructor() {
+        //this.draw()
+    }
+
+    draw = () => {
+        //...
+        this.text()
+        this.drawStation()
+    }
+
+    isPointInStation = (pointerX, pointerY) => {
+        let inside = false
+        inside = this.station.inside(pointerX, pointerY)
+        if (inside) return true
+        return false
+    }
+
+    drawStation = () => {
+        let x
+        let y
+        this.station = this.drawing.circle()
+        this.station.data('sn', this.sn)
+    }
+
+    text = () => {
+        //...
+    }
 }
